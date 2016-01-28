b96108a629823253a90e7d05a3f6787c6058ec5f
Maria <mmorrison1978@gmail.com>
2016-01-28 10:57:45 -0700

    added to app.js

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ keys: [process.env.SESSION_KEY1, process.env.SESSION_KEY2] }))
 
 app.use('/', routes);
 app.use('/users', users);
